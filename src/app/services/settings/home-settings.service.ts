9498e2a195bd925b9f408dcdf213ef6a19a85d4f
jonas-digital <info@jonas-digital.be>
2024-01-31 20:40:04 +0100

    Home standard open

--- src/app/services/settings/home-settings.service.ts
+++ src/app/services/settings/home-settings.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class HomeSettingsService {
-  isOpen: boolean = true;
+  isOpen: boolean = false;
   constructor() { }
 }
